9ca084d79d23ea2eab90aec045160d6142f94e3a
beaver <beavernotacat@gmail.com>
2022-12-21 23:43:54 +1000

    added tems model

--- ikati/team/models.py
+++ ikati/team/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TextNote(models.Model):
+    """A note on a team's board"""
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateField(auto_now=True)
+
+    class Meta:
+        verbose_name = "text note"
+        verbose_name_plural = "text notes"
+
+
+class Team(models.Model):
+    """Teams with users"""
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    description = models.TextField()
+    public_message = models.CharField(max_length=200)
+    notes = models.ManyToManyField(TextNote)
