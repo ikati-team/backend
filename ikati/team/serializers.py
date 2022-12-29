c80c3335fedab2ce33cbc7792cc4910c94cea16e
turleo <turleo@outlook.com>
2022-12-30 09:37:52 +1000

    feature - endpoint for creating teams

--- ikati/team/serializers.py
+++ ikati/team/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from team.models import Team, TeamMember, Invite
 
 from user_profile.serializers import UserSerializer
+from django.contrib.auth.models import User
 
 
 class TeamMemberSerializer(serializers.HyperlinkedModelSerializer):
@@ -11,6 +12,31 @@ class TeamMemberSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = TeamMember
         fields = ['team', 'role', 'user']
+
+
+class CreateTeamSerializer(serializers.HyperlinkedModelSerializer):
+    role = serializers.CharField(write_only=True)
+
+    def get_member_ids(self, team):
+        out = []
+        for i in TeamMember.objects.filter(team=team):
+            out.append(i.id)
+        return out
+
+    def create(self, validated_data):
+        team = Team(
+            name=validated_data['name'],
+            description=validated_data['public_message'],
+            public_message=validated_data['public_message'],
+        )
+        team.save()
+        TeamMember(user=self.context.get('request').user, team=team, role=validated_data['role']).save()
+
+        return team
+
+    class Meta:
+        model = Team
+        fields = ['name', 'public_message', 'role']
 
 
 class TeamSerializer(serializers.HyperlinkedModelSerializer):
