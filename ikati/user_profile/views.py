492747639b5d8cdb0c1a06b0ed3a55209fbefa0d
Beaver <beavernoacat@gmail.com>
2022-12-29 19:03:45 +1000

    feature - dispatcher csrf token

--- ikati/user_profile/views.py
+++ ikati/user_profile/views.py
@@ -3,9 +3,16 @@ from rest_framework import permissions, viewsets
 from django.contrib.auth.models import User
 from user_profile.serializers import UserSerializer
 
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(UserViewSet, self).dispatch(*args, **kwargs)
 
